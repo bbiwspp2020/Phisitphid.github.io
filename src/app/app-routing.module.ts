55d2713103929f2249f55d0a480aed9c8eaafd9a
B'Biw Spp <phisitphod@gmail.com>
2023-06-28 20:07:54 +0700

    update toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ProjectHistoryComponent } from './pages/project-history/project-history.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
-  { path:'',component:HomeComponent }
+  { path:'',component:HomeComponent },
+  { path:'project-history',component:ProjectHistoryComponent},
+  { path:'contact',component:ContactComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProjectHistoryComponent } from './pages/project-history/project-history.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProjectHistoryComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
